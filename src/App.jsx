cb37c6979e8ee68220206eabc9ea157c4935e2a6
Deekshitha-m <deekshitham12052001@gmail.com>
2024-04-05 18:32:04 +0530

    Banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css'
 import NavBar from './Components/NavBar/NavBar'
 import Hero from './Components/Hero/Hero'
 import Products from './Components/Products/Products'
+import TopProducts from './Components/TopProducts/TopProducts'
+import Banner from './Components/Banner/Banner'
+import Subscribe from './Components/Subscribe/Subscibe'
+import Testimonials from './Components/Testimonial/Testimonial'
 
 function App() {
   
@@ -13,6 +17,11 @@ function App() {
       <NavBar/>
       <Hero/>
       <Products/>
+      <TopProducts/>
+      <Banner/>
+      <Subscribe/>
+      <Testimonials/>
+      
     </div>
   )
 }
